e4526830e952ce77f7d77dfd0dbe9428f26cb634
T4T4el7 <ochoaarmando13@gmail.com>
2019-04-27 12:30:14 -0700

    deleted  unused components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Header from "./components/Header";
 import ArrowUp from "./components/ArrowUp"
 import ArrowLeft from "./components/ArrowLeft";
 import ArrowRight from "./components/ArrowRight";
@@ -8,11 +7,7 @@ import DuncanIdaho from "./components/DuncanIdaho";
 import SardaukarElite from "./components/SardaukarElite";
 import SardaukarGrunt from "./components/SardaukarGrunt";
 import BattleMenu from "./components/BattleMenu";
-import Nav from "./components/Nav";
-import Input from "./components/Input";
-import Button from "./components/Button";
 import API from "./utils/API";
-import { RecipeList, RecipeListItem } from "./components/RecipeList";
 import { Container, Row, Col } from "./components/Grid";
 
 class App extends Component {
@@ -45,28 +40,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header/>
+       
         <Container>
-          {/* <Row>
-            <Col size="xs-9 sm-10">
-            
-              <ArrowUp/>
-              <ArrowLeft/>
-              <ArrowRight/>
-                   
-            </Col>
-
-            <Col size="lg-12">
-              
-                <DuncanIdaho/>
-                <BeastHarkonnen/>
-              
-              <div>
-                <SardaukarElite/>
-                <SardaukarGrunt/>
-              </div>
-            </Col>
-          </Row> */}
           <Row>
               <Col size="md-3">
                 <DuncanIdaho/>
@@ -84,65 +59,7 @@ class App extends Component {
               />
             </Row>
         </Container>
-        {/* </Jumbotron> */}
       </div>
-   
-    //   <div>
-    //     <Nav />
-    //     <Jumbotron />
-    //     <Container>
-    //       <Row>
-    //         <Col size="md-12">
-    //           <form>
-    //             <Container>
-    //               <Row>
-    //                 <Col size="xs-9 sm-10">
-    //                   <Input
-    //                     name="recipeSearch"
-    //                     value={this.state.recipeSearch}
-    //                     onChange={this.handleInputChange}
-    //                     placeholder="Search For a Recipe"
-    //                   />
-    //                 </Col>
-    //                 <Col size="xs-3 sm-2">
-    //                   <Header></Header>
-    //                   <Button
-    //                     onClick={this.handleFormSubmit}
-    //                     type="success"
-    //                     className="input-lg"
-    //                   >
-    //                     Search
-    //                   </Button>
-    //                 </Col>
-    //               </Row>
-    //             </Container>
-    //           </form>
-    //         </Col>
-    //       </Row>
-    //       <Row>
-    //         <Col size="xs-12">
-    //           {!this.state.recipes.length ? (
-    //             <h1 className="text-center">No Recipes to Display</h1>
-    //           ) : (
-    //             <RecipeList>
-    //               {this.state.recipes.map(recipe => {
-    //                 return (
-    //                   <RecipeListItem
-    //                     key={recipe.title}
-    //                     title={recipe.title}
-    //                     href={recipe.href}
-    //                     ingredients={recipe.ingredients}
-    //                     thumbnail={recipe.thumbnail}
-    //                   />
-    //                 );
-    //               })}
-    //             </RecipeList>
-    //           )}
-    //         </Col>
-    //       </Row>
-    //     </Container>
-    //   </div>
-    // );
     );
   }
 }
