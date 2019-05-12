bb87fbaef97ce588fd42bc9706480d9d00a20551
T4T4el7 <ochoaarmando13@gmail.com>
2019-05-12 11:40:47 -0700

    skeleton for moving story working as intended

--- client/src/LevelOne.js
+++ client/src/LevelOne.js
@@ -4,22 +4,21 @@ import LevelOne from "./components/LevelOne";
 class Level extends React.Component {
     
     state = {
-        message: [
-            "zero",
-            "first",
-            "second",
-            "third",
-            "forth",
-            "fifth"
-        ]
+        message: `You start your adventrure in the citadel, you come into a street, you can
+        continue forward, go left or right.`,
+        second: `You decide to turn left and you run into a Sardukar guard.`,
+        third: `You diced to walk forward and come to a for in the road.`,
+        forth: `You decide to turn to the right and end up walking into an alleyway, you 
+        find an extra battery pack for your shields system but a you're installing it one Sadukar 
+        Grunt and one Eleite corner you, it's time to fight`
     };
 
     handleClickLeft = () => {
         console.log("you decided to go left");
         console.log(this.state);
         console.log(this.state.message);
-        console.log(this.state.message[1]);
-        let newStoryLine = this.state.message[1];
+        console.log(this.state.second);
+        let newStoryLine = this.state.second;
         console.log(newStoryLine)
         this.setState({message: newStoryLine});
     };
@@ -27,7 +26,7 @@ class Level extends React.Component {
     handleClickRight = () => {
         console.log("this button is right");
         console.log(this.state.message[2]);
-        let newStoryLine = this.state.message[2];
+        let newStoryLine = this.state.forth;
         console.log(newStoryLine)
         this.setState({message: newStoryLine});
 
@@ -35,7 +34,7 @@ class Level extends React.Component {
     
     handleClickUp = () =>  {
         console.log("this button is up");
-        let newStoryLine = this.state.message[3];
+        let newStoryLine = this.state.third;
         console.log(newStoryLine)
         this.setState({message: newStoryLine});
     };
